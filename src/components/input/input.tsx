ed75727b91d2a64aa66370e230ca4926ab1eb12b
rgdlr <rodrigr12@hotmail.com>
2023-03-27 23:28:10 +0200

    feat(components): build input

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,14 +1,20 @@
 import { PropsWithChildren, ReactElement } from 'react';
+import { Label } from '../label/label';
+import './input.css';
 
 export type JSXInputElement = PropsWithChildren<
-	Omit<Omit<Partial<HTMLButtonElement>, 'children'>, 'style'>
+	Omit<Omit<Partial<HTMLInputElement>, 'children'>, 'style'>
 >;
 
-export interface CustomInputElement extends JSXInputElement {}
+export interface CustomInputElement extends JSXInputElement {
+	label?: string;
+}
 
 export function Input(props: CustomInputElement): ReactElement {
+	const { label } = props;
 	return (
 		<>
+			<Label>{label}</Label>
 			<input className='input'></input>
 		</>
 	);
