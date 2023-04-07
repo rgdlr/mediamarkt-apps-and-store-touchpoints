ba4985026ad86e068320028e0378b5b5dffe624f
rgdlr <rodrigr12@hotmail.com>
2023-04-07 20:54:49 +0200

    feat(src): organize imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ContextProvider } from './contexts';
 import { App } from './pages/app';
 import './styles/index.css';
-import { ContextProvider } from './contexts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
